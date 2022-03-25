0d4af6dda4ae8d7a1bd7f424235b967d11c1c2f5
Rifki Sumantri <sumantri.rifki@gmail.com>
2022-03-25 22:48:35 +0700

    Add SearchInput on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import styled from 'styled-components'
+
+import { SearchInput } from './components/SearchInput'
+
+const Wrapper = styled.div`
+  padding: 16px;
+`
 
 function App() {
+  const [searchKeyword, setSearchKeyword] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper className='App'>
+      <SearchInput
+        onChange={(e) => setSearchKeyword(e.target.value)}
+        value={searchKeyword}
+      />
+    </Wrapper>
+  )
 }
 
-export default App;
+export default App
