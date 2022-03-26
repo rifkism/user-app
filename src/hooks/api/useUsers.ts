fb972c0134a8adfc044af0bd4ed7f5932d585ec2
Rifki Sumantri <sumantri.rifki@gmail.com>
2022-03-26 19:24:05 +0700

    Implement debounce for calling users API

--- src/hooks/api/useUsers.ts
+++ src/hooks/api/useUsers.ts
@@ -4,21 +4,27 @@ import axios from 'axios'
 // interfaces
 import { User } from './User'
 
+// hooks
+import { useDebounce } from '../useDebounce'
+
 const useUsers = (keyword: string, genderFilter: string) => {
   const [response, setResponse] = useState<User[]>()
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [error, setError] = useState<string>()
 
+  // Delay API call when user types in the search field
+  const debouncedKeyword = useDebounce(keyword, 700)
+
   const buildUrl = useCallback(() => {
     const params = new URLSearchParams({
-      ...(keyword ? { keyword } : {}),
+      ...(debouncedKeyword ? { keyword: debouncedKeyword } : {}),
       ...(genderFilter ? { gender: genderFilter } : {}),
     })
 
     const finalParams = params ? `?${params.toString()}` : ''
 
     return `https://randomuser.me/api${finalParams}&results=15`
-  }, [genderFilter, keyword])
+  }, [genderFilter, debouncedKeyword])
 
   useEffect(() => {
     const fetchUsers = async () => {
