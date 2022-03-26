f208b9918ced328c24e0779c71bcd4dc74f16cb6
Rifki Sumantri <sumantri.rifki@gmail.com>
2022-03-26 19:04:09 +0700

    Integrate data with Table - WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,66 @@
+import { ChangeEvent, useEffect } from 'react'
+
+// components
 import { FilterPanel } from './components/FilterPanel'
+import { Table } from './components/Table/Table'
+import { TableHead } from './components/Table/TableHeader'
+import { TableRow } from './components/Table/TableRow'
+
+// hooks
+import { useQueryString } from './hooks/useQueryString'
+import { useUsers } from './hooks/api/useUsers'
 
 function App() {
+  const queryParams = new URLSearchParams(window.location.search)
+  const [searchKeyword, setSearchKeyword] = useQueryString(
+    'keyword',
+    queryParams.get('keyword') || ''
+  )
+  const [genderFilter, setGenderFilter] = useQueryString('gender', 'all')
+
+  const { error, isLoading, response } = useUsers(searchKeyword, genderFilter)
+
+  const handleSearchOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value)
+  }
+
+  const handleGenderFilterOnChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setGenderFilter(e.target.value)
+  }
+
+  useEffect(() => {
+    if (error) alert(error)
+  }, [error])
+
   return (
     <div>
-      <FilterPanel />
+      <FilterPanel
+        onGenderFilterChange={handleGenderFilterOnChange}
+        onKeywordSearchChange={handleSearchOnChange}
+        searchKeyword={searchKeyword}
+      />
+      <Table>
+        <thead>
+          <TableHead columnOrder={0}>Name</TableHead>
+          <TableHead columnOrder={1}>Email</TableHead>
+          <TableHead columnOrder={2}>Gender</TableHead>
+        </thead>
+        <tbody>
+          {isLoading ? (
+            <h2>Loading...</h2>
+          ) : (
+            response?.map(({ name, email, gender }) => (
+              <TableRow>
+                <td>
+                  {name.title} {name.first} {name.last}
+                </td>
+                <td>{email}</td>
+                <td>{gender}</td>
+              </TableRow>
+            ))
+          )}
+        </tbody>
+      </Table>
     </div>
   )
 }
