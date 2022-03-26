eb3e6a29941209d304db92e56f171d55da47efb1
Rifki Sumantri <sumantri.rifki@gmail.com>
2022-03-26 17:01:24 +0700

    Update select component interface

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,15 +1,11 @@
-import React, {
-  FC,
-  ReactNode,
-  SelectHTMLAttributes,
-} from 'react'
+import { ChangeEvent, FC, ReactNode } from 'react'
 import styled from 'styled-components'
 
-interface InputProps extends SelectHTMLAttributes<HTMLSelectElement> {
+interface InputProps {
   children: ReactNode
   label: string
   name: string
-  onChange?: () => void
+  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void
 }
 
 const Label = styled.label`
@@ -18,7 +14,7 @@ const Label = styled.label`
 `
 
 const StyledSelect = styled.select<{
-  onChange?: () => void
+  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void
 }>`
   height: 34px;
 `
