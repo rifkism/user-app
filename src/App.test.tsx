ec7b5e7bdd9242bf565f31bb8ad771658055fbdc
Rifki Sumantri <sumantri.rifki@gmail.com>
2022-03-27 19:06:47 +0700

    Addd app unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+import { useUsers } from './hooks/api/useUsers'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('./hooks/api/useUsers', () => {
+  return {
+    useUsers: jest.fn(),
+  }
+})
+
+jest.spyOn(URLSearchParams.prototype, 'get').mockImplementation((key) => key)
+
+test('Should render user data', () => {
+  ;(useUsers as jest.Mock).mockImplementation(() => ({
+    error: null,
+    isLoading: false,
+    users: [
+      {
+        name: { title: 'Mr.', first: 'John', last: 'Doe' },
+        email: 'john.doe@example.com',
+        gender: 'male',
+        phone: '12345',
+      },
+    ],
+  }))
+  render(<App />)
+})
+
+test('Should handle loading state', () => {
+  ;(useUsers as jest.Mock).mockImplementation(() => ({
+    error: null,
+    isLoading: true,
+    users: [],
+  }))
+  render(<App />)
+})
