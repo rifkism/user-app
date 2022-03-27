5778fd3c2d08b41405cfeb9f0a13fc4b996636bb
Rifki Sumantri <sumantri.rifki@gmail.com>
2022-03-27 14:29:10 +0700

    Remove ref

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -2,11 +2,10 @@ import { ReactNode } from 'react'
 
 interface TableRowProps {
   children: ReactNode
-  rowRef: any
 }
 
-const TableRow = ({ rowRef, children }: TableRowProps) => {
-  return <tr ref={rowRef}>{children}</tr>
+const TableRow = ({ children }: TableRowProps) => {
+  return <tr>{children}</tr>
 }
 
 export { TableRow }
